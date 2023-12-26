9e13f7d1fae645ef211736144f6a57229222b452
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2023-12-26 16:35:52 -0300

    Initial Commit

---
+++ FootballStats/FootballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballStatsApp.swift
+//  FootballStats
+//
+//  Created by Gabriel Moraes on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FootballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballStats/Views/StandingTableView.swift
@@ -0,0 +1,8 @@
+//
+//  StandingTableView.swift
+//  FootballStats
+//
+//  Created by Gabriel Moraes on 26/12/23.
+//
+
+import Foundation
